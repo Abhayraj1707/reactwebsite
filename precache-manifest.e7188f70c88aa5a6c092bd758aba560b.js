29b6ce2e4010d7b38647a73a82933c59b73f7e49
Abhay Raj <abhayrajsingh1707@gmail.com>
2020-09-08 20:49:47 +0530

    Updates

---
+++ precache-manifest.e7188f70c88aa5a6c092bd758aba560b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e017b414befd9f17eaa1e71cfdfb106d",
+    "url": "/reactwebsite/index.html"
+  },
+  {
+    "revision": "e8366042cfc1475ff7f6",
+    "url": "/reactwebsite/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "4bbecc5bc9f0919c09a3",
+    "url": "/reactwebsite/static/css/main.9bf578eb.chunk.css"
+  },
+  {
+    "revision": "e8366042cfc1475ff7f6",
+    "url": "/reactwebsite/static/js/2.214849e6.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reactwebsite/static/js/2.214849e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bbecc5bc9f0919c09a3",
+    "url": "/reactwebsite/static/js/main.5286283f.chunk.js"
+  },
+  {
+    "revision": "eec87808e158f3f4700a",
+    "url": "/reactwebsite/static/js/runtime-main.824e2a87.js"
+  },
+  {
+    "revision": "19a2e0eab24e9e902408d4f8547eeab9",
+    "url": "/reactwebsite/static/media/android.19a2e0ea.jpeg"
+  },
+  {
+    "revision": "70cea92af61399debee3717652a63569",
+    "url": "/reactwebsite/static/media/app.70cea92a.jpeg"
+  },
+  {
+    "revision": "ac8f6f9ad22a8902b83e21560b95dca5",
+    "url": "/reactwebsite/static/media/digital.ac8f6f9a.jpeg"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/reactwebsite/static/media/hero-img.619f0cc8.png"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/reactwebsite/static/media/img2.e981619f.svg"
+  },
+  {
+    "revision": "2eed95a2d555c404f1f451730eaa9eef",
+    "url": "/reactwebsite/static/media/marketing.2eed95a2.jpg"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/reactwebsite/static/media/s1.1192a86d.jpg"
+  },
+  {
+    "revision": "43049cae060f2a2a2acb77007f8482f9",
+    "url": "/reactwebsite/static/media/software.43049cae.jpeg"
+  }
+]);
